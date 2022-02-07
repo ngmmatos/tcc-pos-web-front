c1f9c976701fe3eaee38ba5ff65280105c04c6ca
Natã Matos <ngm.matos@gmail.com>
2022-02-07 19:48:39 -0300

    adicao de componentes

--- dev_portfolio/src/components/Layout.jsx
+++ dev_portfolio/src/components/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { SidebarAdm } from './SideBarAdm';
 import { Sidebar } from './Sidebar';
+import { SidebarBarbeiro } from './SidebarBarbeiro';
 import { Header } from './Header';
 import '../scss/layout.scss';
 
 export const Layout = ({ title, children}) => {
     return(
         <div className='container'>
-            <Sidebar />
+            <SidebarAdm />
             <div className='content'>
                 <Header title={title} />
                 {children}
